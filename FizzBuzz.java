e6b8226cb90956add640926647bdeb4e8308ab1d
Chris Caragianis <chriscaragianis@gmail.com>
2015-11-18 10:37:46 -0500

    Add description to FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,16 @@
 class FizzBuzz {
   //Body of FizzBuzz class
-  //will inlude methods for computing a single FizzBuzz number
+
+  //will inlude methods for computing a single
+  //FizzBuzz number
   //and a list of n FizzBuzz numbers
+
+  //fizzNumber(int n)
+  //Computes the proper fizzbuzz output
+  //for a number, n
+
+  //fizzList(int start, int end)
+  //Print to the console
+  //a list of all fizzbuzz outputs starting with
+  //start and ending witn end - 1.
 }
